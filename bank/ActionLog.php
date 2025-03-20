8e66ac18b86fa5caf5f550b16e1bb458de9e46f8
Raj10203 <patilricky39@gmail.com>
2025-03-20 16:59:05 +0530

    display massage improve

--- bank/ActionLog.php
+++ bank/ActionLog.php
@@ -5,9 +5,10 @@ trait ActionLog
     public function add_log($response)
     {
         $myfile = fopen("log.log", "a") or die("Unable to open file!");
-        $txt = "UserName :" . $response['userName'] . ", Action:" . $response['action'] . ", Status: " . $response['status'] . "\n";
+        $txt = "UserName : " . $response['userName'] . ", Action : " . $response['action'] . ", Status : " . $response['status'] . "\n";
         fwrite($myfile, $txt);
         fclose($myfile);
+        echo $txt;
     }
 }
 
--- bank/Current.php
+++ bank/Current.php
@@ -1,26 +1,15 @@
 <?php
 namespace Bank;
-use Bank\Bank;
-use Bank\ActionLog;
 
 class Current extends Bank
 {
-    public $limit;
     use ActionLog;
-    public $response = array();
+    public $limit;
     public function __construct($userName, $balance, $limit = 1000)
     {
         Bank::__construct($userName, $balance);
-        $this->account_type = 'saving';
-        $this->response['userName'] = $userName;
+        $this->accounType = 'Current';
         $this->limit = $limit;
-    }
-    public function deposit($amount)
-    {
-        $this->response['action'] = "deposit of $amount amount";
-        $this->response['status'] = "Success : Successfully deposited.";
-        Bank::$totalTransactions++;
-        $this->balance += $amount;
     }
     public function withdraw($amount)
     {
@@ -31,22 +20,12 @@ class Current extends Bank
                 $this->balance -= $amount;
                 Bank::$totalTransactions++;
             } else {
-                $this->response['status'] = "Error : Withdraw amount exceed limit of your current account";
+                $this->response['status'] = "Error : Withdraw amount exceed limit of your current account. ";
             }
         } else {
             $this->response['status'] = 'Error : Account Balance is less than your withdraw amount';
         }
-        echo $this->response['status'];
         $this->add_log($this->response);
-    }
-
-    public function get_data()
-    {
-        return array(
-            'name' => $this->user_name,
-            'balance' => $this->balance,
-            'account_type' => $this->account_type,
-        );
     }
 }
 ?>
